5f842bf7a58bb839e0b034091704708318a34c8d
jeniadev015 <jeniadev015@jeniadev015>
2018-07-16 15:49:57 +0200

    Appunti Caramellosi

--- WebApp/src/main/webapp/appunti/appunti.js
+++ WebApp/src/main/webapp/appunti/appunti.js
@@ -4,3 +4,46 @@ app.config(function($stateProvider) {
 		templateUrl : "appunti/appunti.html"
 	})
 });
+app.controller("AppuntiCtrl", AppuntiCtrl);
+
+function AppuntiCtrl($scope){
+	
+	$scope.appuntiLista = [];
+	$scope.searchBar = {
+	        "width" : "137.5%"
+	    }
+	
+	$scope.search = function(){
+		//tira fuori dal database 10 elementi;
+		//partendo dalla lunghezza dell'array appuntiLista fino a appuntilista+10
+		console.log("Funzia");
+	}
+	
+	$scope.subjects = [
+		{displayName: "Matematica", subjectId : '1'},
+		{displayName: "Geografia", subjectId: '2'},
+		{displayName: "Italiano", subjectId: '3'},
+		{displayName: "Informatica", subjectId: '4'}
+		];
+	// riccio's work
+	$scope.onClickMateria = function(){
+		console.log("Il click di materia funzia :D");
+	}
+	//kama's work
+	/*
+	 * un appunto ha 
+	 * id
+	 * owner
+	 * subject
+	 * formato
+	 * tag
+	 * professore
+	 * 
+	 */
+	$scope.miaListaAppunti = [];
+	$scope.uploadFile = function(){
+		console.log("ciao da i miei");
+	};
+	
+	
+}
---
+++ WebApp/src/main/webapp/appunti/caricaAppunti/caricaAppunti.js
@@ -0,0 +1,6 @@
+app.config(function($stateProvider) {
+	$stateProvider.state("caricaAppunti", {
+		url : "/caricaAppunti",
+		templateUrl : "appunti/caricaAppunti/caricaAppunti.html"
+	})
+});
