8600acecccaab8124dc1105cd9acee1296257658
jeniadev015 <jeniadev015@jeniadev015>
2018-07-18 08:38:52 +0200

    Continuato caricaAppunti

--- WebApp/src/main/webapp/appunti/caricaAppunti/caricaAppunti.js
+++ WebApp/src/main/webapp/appunti/caricaAppunti/caricaAppunti.js
@@ -1,6 +1,17 @@
-app.config(function($stateProvider) {
+app.config(function($stateProvider){
 	$stateProvider.state("caricaAppunti", {
-		url : "/caricaAppunti",
-		templateUrl : "appunti/caricaAppunti/caricaAppunti.html"
+		url:"/caricaAppunti",
+		templateUrl:"appunti/caricaAppunti/caricaAppunti.html"
 	})
 });
+
+app.controller("caricaAppuntiCtrl", caricaAppuntiCtrl);
+
+function caricaAppuntiCtrl($scope, appuntiService){
+	$scope.compiling = false
+		
+	$scope.goToLoading = function (){
+		compile = true;
+		goto('caricaAppunti');
+		}
+	 }
