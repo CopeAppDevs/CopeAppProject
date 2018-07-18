16fe889a92e3bc058072fa1121b107690e1f68a0
EnJun Hu <enjunhu@gmail.com>
2018-07-18 16:47:39 +0200

    Aggiornamenti vari.

--- WebApp/src/main/webapp/appunti/paginaAppunti/paginaAppunti.js
+++ WebApp/src/main/webapp/appunti/paginaAppunti/paginaAppunti.js
@@ -11,6 +11,7 @@ app.config(function($stateProvider){
 app.controller("paginaAppuntiCtrl", paginaAppuntiCtrl);
 
 function paginaAppuntiCtrl($scope, appuntiService, $stateParams){
+	console.log($stateParams.idAppunto);
 	$scope.appunto=[];
 	
 	var appuntoPromise = appuntiService.getAppunto($stateParams.idAppunto);
