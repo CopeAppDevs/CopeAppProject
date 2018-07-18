9680af6445c6e1af63311f635fb6ec25ad197a7b
jeniadev015 <jeniadev015@jeniadev015>
2018-07-18 17:15:12 +0200

    vedo le card nella main page bug nell'aggiorna

--- WebApp/src/main/webapp/appunti/iMieiAppunti/iMieiAppunti.js
+++ WebApp/src/main/webapp/appunti/iMieiAppunti/iMieiAppunti.js
@@ -1,14 +1,28 @@
-app.config(function($stateProvider){
+app.config(function($stateProvider) {
 	$stateProvider.state("iMieiAppunti", {
-		url:"iMieiAppunti",
-		templateUrl:"appunti/iMieiAppunti/iMieiAppunti.html",
+		url : "iMieiAppunti",
+		templateUrl : "appunti/iMieiAppunti/iMieiAppunti.html",
 	})
 });
 
 app.controller("iMieiAppuntiCtrl", iMieiAppuntiCtrl);
 
-function iMieiAppuntiCtrl($scope, appuntiService){
-		
+function iMieiAppuntiCtrl($scope, appuntiService) {
+
 	$scope.laMiaLista = [];
-          
+
+	var listaPromise = appuntiService.search($scope.text);
+	listaPromise.then(function onSuccess(searchResponse) {
+		$scope.laMiaLista = searchResponse.data.appunti;
+	}, $scope.serverErrorCallbackToast);
+
+	$scope.refreshMine = function() { //TODO raddoppia gli appunti ??? WHAT
+		appuntiService.search($scope.user, false, true, $scope.laMiaLista.length)
+		.then(function(response) {
+			for (var a = 0; a < response.data.appunti.length; a++) {
+				$scope.laMiaLista.push(response.data.appunti[a]);
+			}
+		}, $scope.serverErrorCallback);
+	}
+
 }
