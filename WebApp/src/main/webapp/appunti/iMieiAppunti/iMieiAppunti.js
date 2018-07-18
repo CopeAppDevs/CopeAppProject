ecf44334a7edd19d75e73e7ce5d6cf41169f084f
jeniadev015 <jeniadev015@jeniadev015>
2018-07-18 10:31:03 +0200

    Fatte alcune modifiche su caricaAppunti

---
+++ WebApp/src/main/webapp/appunti/iMieiAppunti/iMieiAppunti.js
@@ -0,0 +1,13 @@
+//app.config(function($stateProvider){
+//	$stateProvider.state("caricaAppunti", {
+//		url:"/caricaAppunti",
+//		templateUrl:"appunti/caricaAppunti/caricaAppunti.html"
+//	})
+//});
+
+app.controller("iMieiAppuntiCtrl", iMieiAppuntiCtrl);
+
+function iMieiAppuntiCtrl($scope, appuntiService){
+	
+             
+}
