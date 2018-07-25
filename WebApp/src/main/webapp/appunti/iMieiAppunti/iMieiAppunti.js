c901a53b1db9e5054bbde382fff09a4a272dddb0
jeniadev015 <jeniadev015@jeniadev015>
2018-07-25 12:11:46 +0200

    pushhhhhhh

--- WebApp/src/main/webapp/appunti/iMieiAppunti/iMieiAppunti.js
+++ WebApp/src/main/webapp/appunti/iMieiAppunti/iMieiAppunti.js
@@ -10,17 +10,18 @@ app.controller("iMieiAppuntiCtrl", iMieiAppuntiCtrl);
 function iMieiAppuntiCtrl($scope, appuntiService) {
 
 	$scope.laMiaLista = [];
+	$scope.indexToRetrive =0;
 
 	var listaPromise = appuntiService.search($scope.user, "" , true, 0, 5);
 	listaPromise.then(function onSuccess(searchResponse) {
-		$scope.laMiaLista = searchResponse.data.appunti;
+		$scope.laMiaLista = searchResponse.data.appuntoMini;
 	}, $scope.serverErrorCallbackToast);
 
 	$scope.refreshMine = function() {
 		appuntiService.search($scope.user, "" , true, 0, 5)
 		.then(function(response) {
-			for (var a = 0; a < response.data.appunti.length; a++) {
-				$scope.laMiaLista.push(response.data.appunti[a]);
+			for (var a = 0; a < response.data.appuntoMini.length; a++) {
+				$scope.laMiaLista.push(response.data.appuntoMini[a]);
 			}
 		}, $scope.serverErrorCallback);
 	}
