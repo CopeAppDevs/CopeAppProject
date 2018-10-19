5b551823089d72d8d796fa36f753067a87304329
FabioTessaro <fabio.tessaro.porta@gmail.com>
2018-10-19 19:11:48 +0200

    Test mongoose

--- repositories/database.coffee
+++ repositories/database.coffee
@@ -1,17 +1,67 @@
 colors = require("colors")
 mongoose = require("mongoose")
-
-exports.mongoose = mongoose
+shoolSchema = require("../schemas/school")
 
 connect = (dbHost) ->
   mongoose.connect('mongodb://'+dbHost+'/local', {
     useNewUrlParser: true
-  }).then(() ->
-    console.log(colors.green("Connected to mongodb at "+dbHost))
-  ,(err) ->
-    console.error(colors.red("failed to connect to db at "+dbHost))
-    console.error(colors.red(err))
-  )
+  })
 
 exports.createDatabase = (dbHost) ->
+
+  populateDatabase = () ->
+    console.log(colors.green("Populating..."))
+    copernico = new shoolSchema.School({
+
+      schoolName: "Copernico",
+      address: "Via garavaglia 7",
+      phone: "335unguunguungu",
+      mail: "copernico]ungu.it",
+      website: "www.copernico.bo.it",
+
+      students: [{
+        firstName: "claudia",
+        lastName: "unguendoli",
+        username: "ungukla",
+        password: "VincioGay",
+        email: "ungu@ungu.it",
+        roles: [{
+          role: "student",
+          description: "ungu"
+        }]
+      }],
+      teachers: [{
+        firstName: "claudio",
+        lastName: "unguendoli",
+        username: "ungukla",
+        password: "VincioGay",
+        email: "ungu@ungu.it",
+        roles: [{
+          role: "teacher",
+          description: "ungu"
+        },{
+          role: "ungu",
+          description: "ungu"
+        }]
+      }],
+      classes: [{
+        section: "A",
+        year: 2,
+        subject: "linguistico",
+        location: "412"
+      }]
+    })
+    copernico.save((err, savedObj) ->
+      if err
+        console.error(colors.red(err))
+    )
+    console.log(colors.green("Populated"))
+
   connection = connect(dbHost)
+  connection.then(() ->
+      console.log(colors.green("Connected to mongodb at "+dbHost))
+      populateDatabase()
+    ,(err) ->
+      console.error(colors.red("failed to connect to db at "+dbHost))
+      console.error(colors.red(err))
+    )
--- repositories/database.js
+++ repositories/database.js
@@ -1,23 +1,83 @@
-var colors, connect, mongoose;
+var colors, connect, mongoose, shoolSchema;
 
 colors = require("colors");
 
 mongoose = require("mongoose");
 
-exports.mongoose = mongoose;
+shoolSchema = require("../schemas/school");
 
 connect = function(dbHost) {
   return mongoose.connect('mongodb://' + dbHost + '/local', {
     useNewUrlParser: true
-  }).then(function() {
-    return console.log(colors.green("Connected to mongodb at " + dbHost));
-  }, function(err) {
-    console.error(colors.red("failed to connect to db at " + dbHost));
-    return console.error(colors.red(err));
   });
 };
 
 exports.createDatabase = function(dbHost) {
-  var connection;
-  return connection = connect(dbHost);
+  var connection, populateDatabase;
+  populateDatabase = function() {
+    var copernico;
+    console.log(colors.green("Populating..."));
+    copernico = new shoolSchema.School({
+      schoolName: "Copernico",
+      address: "Via garavaglia 7",
+      phone: "335unguunguungu",
+      mail: "copernico]ungu.it",
+      website: "www.copernico.bo.it",
+      students: [
+        {
+          firstName: "claudia",
+          lastName: "unguendoli",
+          username: "ungukla",
+          password: "VincioGay",
+          email: "ungu@ungu.it",
+          roles: [
+            {
+              role: "student",
+              description: "ungu"
+            }
+          ]
+        }
+      ],
+      teachers: [
+        {
+          firstName: "claudio",
+          lastName: "unguendoli",
+          username: "ungukla",
+          password: "VincioGay",
+          email: "ungu@ungu.it",
+          roles: [
+            {
+              role: "teacher",
+              description: "ungu"
+            }, {
+              role: "ungu",
+              description: "ungu"
+            }
+          ]
+        }
+      ],
+      classes: [
+        {
+          section: "A",
+          year: 2,
+          subject: "linguistico",
+          location: "412"
+        }
+      ]
+    });
+    copernico.save(function(err, savedObj) {
+      if (err) {
+        return console.error(colors.red(err));
+      }
+    });
+    return console.log(colors.green("Populated"));
+  };
+  connection = connect(dbHost);
+  return connection.then(function() {
+    console.log(colors.green("Connected to mongodb at " + dbHost));
+    return populateDatabase();
+  }, function(err) {
+    console.error(colors.red("failed to connect to db at " + dbHost));
+    return console.error(colors.red(err));
+  });
 };
